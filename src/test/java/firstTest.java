56a08a60786aba2d358ef2f4ae63ba3417d909e0
Anna Madera <anna.madera7@gmail.com>
2020-09-29 19:11:23 +0200

    firstTest

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstTest {
+    @Test
+    public void demoTest(){
+        System.setProperty( "webdriver.chrome.driver","src/main/resources/chromedriver.exe" );
+        WebDriver driver= new ChromeDriver();
+        driver.navigate().to("https://google.pl");
+
+    }
+}
